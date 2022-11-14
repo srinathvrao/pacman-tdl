21b64bfa453ea96783b5ce6f40f2d797ebfb4012
Srinath Venkobarao <svenkoba@asu.edu>
2022-11-14 00:51:59 -0700

    Handled Unreachable Locations

---
+++ code/environmentAugmenter.py
@@ -0,0 +1,88 @@
+import random
+import numpy as np
+
+def layoutCorrector(layout, agent_position):
+    '''
+        Layout Correction:
+            - layout: h x w 2d list of the interior
+            - agent_position: (x,y) of the agent
+
+        The layout we make with the genLayout() function might have 
+            unreachable locations blocked by walls.
+        
+        The correctLayout() function below detects this situation, 
+            and makes these locations reachable.
+
+        Implementation: BFS to check reachability, and 
+                        BFS again to make a navigable path
+    '''
+
+    h, w = len(layout), len(layout[0])
+    numSpaces = sum([ True if layout[p][q] in [' ','.','P','G'] else False for p in range(h) for q in range(w)])
+    
+    queue = [agent_position]
+    visited = np.zeros((h,w)).astype(np.uint8)
+
+    # BFS to verify there are no unreachable spaces.
+    while len(queue) > 0:
+        x,y = queue.pop(0)
+        if not visited[y,x]:
+            visited[y,x] = 1
+            # add the valid neighbors to the queue
+            coords = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+            for coord in coords:
+                xn, yn = coord
+                if xn<0 or xn>=w or yn<0 or yn>=h:
+                    pass
+                elif layout[yn][xn] != '%':
+                    queue.append((xn,yn))
+
+    # if we are able to visit all the spaces, then we don't need to change the layout
+    numVisited = visited.sum()
+    if numVisited == numSpaces:
+        return layout
+    
+    for y in range(h):
+        for x in range(w):
+            if visited[y,x] == 0:
+                if layout[y][x] == '%':
+                    visited[y,x] = 2
+                elif layout[y][x] == 'G':
+                    visited[y,x] = 3
+
+    unvisited_list = list(zip(*np.where(visited == 0))) # get all unvisited spaces
+
+    for u in unvisited_list:
+        print("Location",u,"is unreachable. Modifying layout..")
+        # BFS for removing walls that block the path
+        queue = [(u,[])]
+        foundPath = False
+        nav_path = []
+        unvis = np.zeros((h,w)).astype(np.uint8)
+        while len(queue)>0 and not foundPath:
+            qp = queue.pop(0)
+            y,x = qp[0]
+            if not unvis[y,x]:
+                unvis[y,x] = 1
+                # add the valid neighbors to the queue
+                coords = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+                for coord in coords:
+                    xn, yn = coord
+                    if xn<0 or xn>=w or yn<0 or yn>=h:
+                        pass
+                    else:
+                        path = qp[1].copy()
+                        path.append((y,x))
+                        if visited[yn,xn]==1:
+                            nav_path = path.copy()
+                            foundPath = True
+                            break
+                        queue.append(( (yn,xn), path ))
+
+        for coord in nav_path:
+            y,x = coord
+            visited[y,x] = 1
+            if layout[y][x]=='%':
+                layout[y][x] = random.choice(['.',' '])
+
+    return layout
--- code/environmentGenerator.py
+++ code/environmentGenerator.py
@@ -1,27 +1,113 @@
 import random
-def genLayout(x,y):
-    for length in range(1,y+1):
-        if(length==1 or length==y):
-            for _ in range(x):
-                print("%",end='')
-            print('')
-        else:
-            print("%",end='')
-            for _ in range(x-2):
-                if random.randint(0,9)%5==0:
-                    print(".",end="")
-                elif random.randint(0,9)%5==1:
-                    print("%",end="")
-                else:
-                    print(" ",end='')
-            print("%")
+import numpy as np
+from optparse import OptionParser
+from environmentAugmenter import layoutCorrector
+import sys
+
+numGhosts = 2 # can be changed from command line
+numPacman = 1 # not implemented for >1 pacman agent
+
+# It is assumed Pacman always has Agent ID 0, and ghosts can take every other ID.
+
+def genLayout(width, height, numGhosts):
+    
+    '''
+        Layout Generation:
+            - width, height of the game.
+            - numGhosts: number of ghosts in the game.
+            - numPacman not implemented (assumed to be 1).
+
+        Creates a simple random layout with the given width and height.
+            - interiors (walls / free space / dots) are assigned probabilities
+                of occurrence.
+
+        Returns a layout of the given dimensions.
+    '''
+
+    interior = []
+    interior.extend([" "]*3) # 30% free space
+    interior.extend(["%"]*2) # 20% walls
+    interior.extend(["."]*5) # 50% food dots
+    
+    layout = []
+    possible_agent_positions = []
+
+    for y in range(height):
+        row = []
+        for x in range(width):
+            fill = random.choice(interior)
+            if fill==' ': possible_agent_positions.append((x,y))
+            row.append(fill)
+        layout.append(row)
+
+    agent_positions = random.sample(possible_agent_positions, numGhosts + numPacman)
+    pacman_position = agent_positions[0]
+    ghost_positions = agent_positions[1:]
+    pac_x, pac_y = pacman_position
+    layout[pac_y][pac_x] = 'P'
+
+    for ghost_position in ghost_positions:
+        ghost_x, ghost_y = ghost_position
+        layout[ghost_y][ghost_x] = 'G'
+
+    return layout, pacman_position, ghost_positions
+
+def wrapLayout(layout):
+    
+    #   wraps the layout with walls
+
+    h,w = len(layout),len(layout[0])
+    lay = [['%']*(w+2)]
+    for y in range(h):
+        row = ['%']
+        row.extend(layout[y])
+        row.append('%')
+        lay.append(row)
+    lay.append(['%']*(w+2))
+    return lay
+
+
+def createLayout(width, height, numGhosts):
+    
+    layout, pac_pos, ghost_positions = genLayout(width-2,height-2,numGhosts)
+    layout = layoutCorrector(layout, pac_pos)
+    for ghost_pos in ghost_positions:
+        layout = layoutCorrector(layout, ghost_pos)
+
+    return wrapLayout(layout)
+
+
+if __name__ == "__main__":
+
+    usageStr = """
+    USAGE:      python environmentGenerator.py <options>
+    EXAMPLES:   (1) python environmentGenerator.py
+                    - creates a random 10x10 pacman game layout, with 2 ghosts.
+                (2) python pacman.py -w 20 -l 25 -g 5
+                    - creates a pacman game layout with width 20, height 25, and 5 ghost agents
+    """
+    parser = OptionParser(usageStr)
+
+    parser.add_option('-w', '--width', dest='width', type='int',
+                      help='the width of the maze', metavar='WIDTH', default=10)
+    parser.add_option('-l', '--height', dest='height', type='int',
+                      help='the height of the maze', metavar='HEIGHT', default=10)
+    parser.add_option('-g', '--numGhosts', dest='numGhosts', type='int',
+                      help='the number of ghost agents in the game', metavar='NUMGHOSTS', default=2)
     
 
+    options, otherjunk = parser.parse_args(sys.argv[1:])
 
+    if len(otherjunk) != 0:
+        raise Exception('Command line input not understood: ' + str(otherjunk))
+    
+    mazeWidth = options.width
+    mazeHeight = options.height
+    numGhosts = options.numGhosts
+    numPacman = 1 # not implemented for >1 pacman agent
 
-genLayout(100,300)
+    layout = createLayout(mazeWidth, mazeHeight, numGhosts)
 
-# i=1
-# while(i<10):
-#     envString = "envgen{}.lay".format(i)
-#     x = open(envString,"w")
+    for row in layout:
+        print("".join(row))
+
